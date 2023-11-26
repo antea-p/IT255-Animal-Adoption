0186d67a82dc571e0344056fc234a6c78da9b589
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2023-11-26 16:10:14 +0100

    added basic endpoints (home, details, about us)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    // component: HomeComponent
+    title: 'Home page',
+  },
+  {
+    path: 'detail/:id',
+    // component: AnimalDetailsComponent
+    title: 'Details page',
+  },
+  {
+    path: 'about-us',
+    // component: AboutUsComponent
+    title: 'About Us page',
+  },
+];
+
+export default routes;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
