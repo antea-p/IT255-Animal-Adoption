c4674fe052f6d708e7514adce2d7c77c1df82bda
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2023-11-26 21:34:46 +0100

    created home component and updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: AppComponent,
-  //   title: 'Home page',
-  // },
-
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
   {
     path: 'about-us',
     component: AboutUsComponent,
     title: 'About Us page',
   },
 ];
-
-export default routes;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
