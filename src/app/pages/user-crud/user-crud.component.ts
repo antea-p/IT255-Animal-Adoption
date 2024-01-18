514ede93d612891da00cf999eec24828b3fc47d4
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-01-18 21:37:41 +0100

    improved form

--- src/app/pages/user-crud/user-crud.component.ts
+++ src/app/pages/user-crud/user-crud.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
@@ -10,6 +10,7 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user-crud.component.css']
 })
 export class UserCrudComponent implements OnInit {
+  // TODO: onemogući brisanje trenutnog admina
   users: User[] = [];
   newUserForm = new FormGroup({});
   userModel: User = {
@@ -22,22 +23,31 @@ export class UserCrudComponent implements OnInit {
     {
       key: 'email',
       type: 'input',
-      templateOptions: {
-        type: 'email',
+      props: {
         label: 'Email',
-        placeholder: 'Enter email',
+        type: 'email',
         required: true,
-      }
+        pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+      },
+      validation: {
+        messages: {
+          pattern: () => `Email must be a valid email address! (e.g. myname@gmail.com)`,
+        },
+      },
     },
     {
       key: 'password',
       type: 'input',
-      templateOptions: {
-        type: 'password',
-        label: 'Password',
-        placeholder: 'Enter password',
+      props: {
+        label: 'Password (min. 6 characters)',
         required: true,
-      }
+        minLength: 6
+      },
+      validation: {
+        messages: {
+          minLength: () => `Password must be at least 6 characters long`,
+        },
+      },
     }
   ];
 
@@ -69,6 +79,11 @@ export class UserCrudComponent implements OnInit {
   }
 
   createOrUpdateUser(): void {
+    if (!this.newUserForm.valid) {
+      console.log('Form is not valid!');
+      return;
+    }
+
     if (this.userModel.id) {
       console.log('Updating user:', this.userModel);
 
